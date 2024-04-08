4562886ddf93a17e47de31185dfe24f955cc1a52
Polan <gemini6879@gmail.com>
2024-04-07 22:35:28 -0400

    Layout Update

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -1,16 +1,16 @@
-import { Box, Card, CardMedia, Divider, Grid, ImageList, Link, List, ListItem, ListItemIcon, Stack, Typography } from '@mui/material'
-import MPLogo from '../mp-logo'
+import { CardMedia, Divider, Grid, List, ListItem, Stack, Typography } from '@mui/material'
 import profilePic from '../../assets/profilePic.jpg';
 import './resume.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { forwardRef } from 'react';
+import SocialIcon from '../socialIcon';
 
 type Props = {
 };
 
 const Resume = forwardRef<HTMLDivElement, Props>((props, ref) => {
 	return (
-		<Stack direction='row' className="resume-main" ref={ref}>
+		<Grid direction='row' className="resume-main" ref={ref} spacing={2}>
 			<Stack className='contact' sx={{ mt: '140px', ml: '70px' }}>
 				<Stack sx={{ m: '26px' }}>
 					<CardMedia
@@ -20,62 +20,55 @@ const Resume = forwardRef<HTMLDivElement, Props>((props, ref) => {
 					/>
 					<Typography sx={{ m: '12px' }}>FULL STACK DEVELOPER</Typography>
 				</Stack>
-				<Stack className='side-info'>
-					<Stack className='contact-info' spacing={3}>
-						<Box className='contact-icon'>
-							<FontAwesomeIcon icon='phone' />
-							<Typography variant='body1'>717.318.6104</Typography>
-						</Box>
-						<Box>
-							<FontAwesomeIcon icon={{ prefix: 'fab', iconName: 'github' }} />
-							<Typography variant='body1'>
-								<Link href='https://github.com/TinyPilot06'>https://github.com/TinyPilot06</Link>
-							</Typography>
-						</Box >
-						<Box>
-							<FontAwesomeIcon icon={{ prefix: 'fab', iconName: 'linkedin' }} />
-							<Typography variant='body1'>
-								<Link href='https://www.linkedin.com/in/michelle-polan'>https://www.linkedin.com/in/michelle-polan</Link>
-							</Typography>
-						</Box>
-						<Box>
-							<FontAwesomeIcon icon='globe' />
-							<Typography variant='body1'>
-								<Link href='http://www.michellepolan.com'>http://www.michellepolan.com</Link>
-							</Typography>
-						</Box>
+				<Stack>
+					<Stack className='contact-info' direction="row">
+						<Stack className='info' spacing={2}>
+							<Stack direction='row' alignContent='center' spacing={4}>
+								<SocialIcon url="https://www.linkedin.com/in/michelle-polan/" icon={{ prefix: 'fab', iconName: 'linkedin' }} />
+								<Typography>LinkedIn</Typography>
+							</Stack>
+							<Stack direction='row' alignContent='center' spacing={4}>
+								<SocialIcon url="https://github.com/TinyPilot06" icon={{ prefix: 'fab', iconName: 'github' }} />
+								<Typography>GitHub</Typography>
+							</Stack>
+							<Stack direction='row' alignContent='center' spacing={4}>
+								<FontAwesomeIcon icon='phone' />
+								<Typography variant='body1'>717.318.6104</Typography>
+							</Stack>
+						</Stack>
 					</Stack>
+
 					<Stack className='contact-info'>
-						<Stack className='contact-item-title'>TECHNICAL SKILLS</Stack>
-						{/* sx={{ mt: '200px', ml: '60px', mb: '20px' }} */}
-						<Stack>
-							<List>
-								<ListItem>
-									Languages
-									JavaScript/TypeScript, Python, HTML, CSS, SASS
-								</ListItem>
-								<ListItem>
-									Frameworks & Technologies <br />
-									React, Node.js, Django, MongoDB, MUI, AWS Lambda, Docker
-								</ListItem>
-								<ListItem>
-									Databases <br />
-									PostgreSQL, DynamoDB
-								</ListItem>
-								<ListItem>
-									Tools <br />
-									Git, Docker
-								</ListItem>
-							</List>
+						<Stack className='contact-item-title' sx={{ m: '6px', p: '8px' }}>TECHNICAL SKILLS</Stack>
+						<Stack spacing={3}>
+							<Typography>
+								Languages <br />
+								JavaScript/TypeScript, Python, HTML, CSS, SASS<br />
+							</Typography>
+							<Typography>
+								Frameworks & Technologies <br />
+								React, Node.js, Django, MongoDB, MUI, AWS Lambda, Docker < br />
+							</Typography>
+							<Typography>
+								Databases <br />
+								PostgreSQL, DynamoDB <br />
+							</Typography>
+							<Typography>
+								Tools <br />
+								Git, Docker
+							</Typography>
 						</Stack>
 					</Stack>
 					<Stack className='contact-info'>
-						<Stack className='contact-item-title'>CERTIFICATION AND COURSES</Stack>
-						{/* sx={{ mt: '200px', ml: '40px', mb: '20px' }} */}
-						<Stack sx={{ m: '12px' }}>
+						<Stack className='contact-item-title' sx={{ m: '6px', p: '8px' }}>CERTIFICATION AND COURSES</Stack>
+						<Stack spacing={3} sx={{ m: '6px', p: '8px' }}>
 							<Typography>
 								Web Development Fundamentals, nuCamp <br />
+							</Typography>
+							<Typography>
 								Backend, SQL, and DevOps with Python, nuCamp <br />
+							</Typography>
+							<Typography>
 								Front End and Mobile Development, nuCamp
 							</Typography>
 						</Stack>
@@ -94,26 +87,30 @@ const Resume = forwardRef<HTMLDivElement, Props>((props, ref) => {
 						</Typography>
 					</Stack>
 				</Stack>
-				<Stack className='resume-items'>
+				<Divider variant='middle' />
+				<Stack>
 					<Stack className='resume-item-title'>PROJECTS</Stack>
 					<Typography className='resume-text'>
 						Music Library WebApp <br />
 						React, TypeScript, Material UI, Node.js, Cognito, Lambda, DynamoDB, S3<br />
 						<br />
 						Developed a full-featured music library management application from scratch, focusing on serverless architecture with AWS Lambda functions and DynamoDB for data management. Front end built in React using Material UI v5.
-						Command Line Blackjack <br />
 						<br />
+						<br />
+						Command Line Blackjack <br />
 						Python
 						<br />
-						Created a command line-based Blackjack game with full Blackjack rules logic and ASCII graphics.
+						Created a command line-based Blackjack game with full Blackjack rules logic and ASCII graphics. <br />
+						<br />
 						NuCamp Bootcamp <br />
 						HTML, CSS, JavaScript, SQL, React, ReactNative, AWS/Azure DevOps, <br />
 						Participated in numerous projects, covering website and web app design & development, database development, Python scripting, DevOps in both the AWS and Azure environments, mobile app development, and full-stack design and architecture principles.
 					</Typography>
 				</Stack>
-				<Stack className='resume-items' spacing={4}>
+				<Divider variant='middle' />
+				<Stack className='resume-items' spacing={3}>
 					<Stack className='resume-item-title'>PROFESSIONAL EXPERIENCE</Stack>
-					<Stack>
+					<Stack spacing={3}>
 						<List>
 							<ListItem>
 								<Stack direction='row' spacing='600px'>
@@ -180,6 +177,7 @@ const Resume = forwardRef<HTMLDivElement, Props>((props, ref) => {
 						</List>
 					</Stack>
 				</Stack>
+				<Divider variant='middle' />
 				<Stack sx={{ mt: '16px', mb: '16px' }} className='resume-items'>
 					<Stack className='resume-item-title' >SOFT SKILLS</Stack>
 					<Typography className='resume-text'>
@@ -188,6 +186,7 @@ const Resume = forwardRef<HTMLDivElement, Props>((props, ref) => {
 						Adaptability and eagerness to learn and apply new technologies.
 					</Typography>
 				</Stack>
+				<Divider variant='middle' />
 				<Stack className='resume-items'>
 					<Stack className='resume-item-title'>ACTIVITIES AND INTERESTS</Stack>
 					<Typography className='resume-text '>
@@ -197,7 +196,7 @@ const Resume = forwardRef<HTMLDivElement, Props>((props, ref) => {
 					</Typography>
 				</Stack>
 			</Stack>
-		</Stack >
+		</Grid >
 	)
 });
 
